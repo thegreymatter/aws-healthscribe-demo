f05bc0268607d32f5a1f105063eb60a132085be3
omrif <omrif@diagnosticrobotics.com>
2024-04-23 13:28:08 +0300

    raise

--- src/store/auth/index.tsx
+++ src/store/auth/index.tsx
@@ -66,16 +66,11 @@ export default function AuthContextProvider({ children }: { children: React.Reac
             if (data?.payload?.event) getAuthUser().catch(console.error);
         }
         async function getAuthUser() {
-            try {
                 const ccredentials = await Auth.currentCredentials();
                 const cuser = await Auth.currentAuthenticatedUser();
                 setCredentails(ccredentials);
                 setUser(cuser);
                 console.log(ccredentials);
-            } catch {
-                console.log('eeerr');
-                setCredentails(false);
-            }
         }
         getAuthUser().catch(console.error);
         Hub.listen('auth', authListener);
